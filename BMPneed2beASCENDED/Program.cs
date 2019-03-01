a029496a05bc22b0d700ae8d7b184bcbb2f2e2a1
Party4Bread <solsam10@naver.com>
2019-03-01 12:19:59 +0900

    hey it works!

--- BMPneed2beASCENDED/Program.cs
+++ BMPneed2beASCENDED/Program.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Console = Colorful.Console;
-using Colorful;
+//using Console = Colorful.Console;
+//using Colorful;
 using System.IO;
 using System.Drawing;
 using System.Runtime.InteropServices;
@@ -18,6 +18,7 @@ namespace BMPneed2beASCENDED
 {
     class Program
     {
+        #region p/invoke things
         [StructLayout(LayoutKind.Sequential)]
         public struct RECT
         {
@@ -25,6 +26,32 @@ namespace BMPneed2beASCENDED
             public int Top;         // y position of upper-left corner
             public int Right;       // x position of lower-right corner
             public int Bottom;      // y position of lower-right corner
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct COORD
+        {
+            public short X;
+            public short Y;
+
+            public COORD(short X, short Y)
+            {
+                this.X = X;
+                this.Y = Y;
+            }
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct CONSOLE_FONT_INFO_EX
+        {
+            public uint cbSize;
+            public uint nFont;
+            public COORD dwFontSize;
+            public int FontFamily;
+            public int FontWeight;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public string FaceName;
         }
 
         [DllImport("user32.dll", SetLastError = true)]
@@ -36,6 +63,46 @@ namespace BMPneed2beASCENDED
         [DllImport("user32.dll", SetLastError = true)]
         static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern Int32 SetCurrentConsoleFontEx(IntPtr ConsoleOutput,bool MaximumWindow,ref CONSOLE_FONT_INFO_EX ConsoleCurrentFontEx);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        extern static bool GetCurrentConsoleFontEx(IntPtr hConsoleOutput,bool bMaximumWindow,ref CONSOLE_FONT_INFO_EX lpConsoleCurrentFont);
+        
+        private enum StdHandle
+        {
+            OutputHandle = -11
+        }
+
+        [DllImport("kernel32")]
+        private static extern IntPtr GetStdHandle(StdHandle index);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool SetConsoleMode(IntPtr hConsoleHandle, ConsoleModes dwMode);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool GetConsoleMode(IntPtr hConsoleHandle, out ConsoleModes lpMode);
+
+        [Flags]
+        private enum ConsoleModes : uint
+        {
+            ENABLE_PROCESSED_INPUT = 0x0001,
+            ENABLE_LINE_INPUT = 0x0002,
+            ENABLE_ECHO_INPUT = 0x0004,
+            ENABLE_WINDOW_INPUT = 0x0008,
+            ENABLE_MOUSE_INPUT = 0x0010,
+            ENABLE_INSERT_MODE = 0x0020,
+            ENABLE_QUICK_EDIT_MODE = 0x0040,
+            ENABLE_EXTENDED_FLAGS = 0x0080,
+            ENABLE_AUTO_POSITION = 0x0100,
+
+            ENABLE_PROCESSED_OUTPUT = 0x0001,
+            ENABLE_WRAP_AT_EOL_OUTPUT = 0x0002,
+            ENABLE_VIRTUAL_TERMINAL_PROCESSING = 0x0004,
+            DISABLE_NEWLINE_AUTO_RETURN = 0x0008,
+            ENABLE_LVB_GRID_WORLDWIDE = 0x0010
+        }
+        #endregion
         static void Main(string[] args)
         {
             Console.Title = "Realtime Touhou! by P4B";
@@ -52,25 +119,41 @@ namespace BMPneed2beASCENDED
             */
             Bitmap basePic = new Bitmap(Image.FromFile($"badapplefr\\{1:D10}.jpg"));
             int width = 100, height = basePic.Height * width / basePic.Width;
-            Console.SetWindowSize(width * 2 + 1, height+2);
-            IntPtr hwndhwnd = GetConsoleWindow();
-            SetWindowPos(hwndhwnd, IntPtr.Zero, 
-                Screen.AllScreens[0].Bounds.Left, Screen.AllScreens[0].Bounds.Top, 1337, 1337, 1);
 
-            RECT ks = new RECT();
-            GetWindowRect(hwndhwnd, out ks);
-            Rectangle bounds = new Rectangle(ks.Left, ks.Top, ks.Right - ks.Left, ks.Bottom - ks.Top);
+            CONSOLE_FONT_INFO_EX ConsoleFontInfo = new CONSOLE_FONT_INFO_EX();
+            var hnd = GetStdHandle(StdHandle.OutputHandle);
+            ConsoleFontInfo.cbSize = (uint)Marshal.SizeOf(ConsoleFontInfo);
+            ConsoleModes mod;
+            GetConsoleMode(hnd, out mod);
+            SetConsoleMode(hnd, mod & ~(ConsoleModes.ENABLE_AUTO_POSITION|ConsoleModes.ENABLE_MOUSE_INPUT));
+            GetCurrentConsoleFontEx(hnd, false, ref ConsoleFontInfo);
+            ConsoleFontInfo.dwFontSize.Y = 5;
+            Console.CursorVisible = false;
+            SetCurrentConsoleFontEx(hnd, false, ref ConsoleFontInfo);
+            int bufsize = 10000 - 1;
+            Console.SetWindowSize(width * 2, height);
+            Console.SetBufferSize(bufsize, bufsize);
+            Console.ResetColor();
+            //Console.ReplaceAllColorsWithDefaults();
+            //Console.BackgroundColor = Color.White;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //IntPtr hwndhwnd = GetConsoleWindow();
+            //SetWindowPos(hwndhwnd, IntPtr.Zero, Screen.AllScreens[0].Bounds.Left, Screen.AllScreens[0].Bounds.Top, 1337, 1337, 1);
+
+            //RECT ks = new RECT();
+            //GetWindowRect(hwndhwnd, out ks);
+            //Rectangle bounds = new Rectangle(ks.Left, ks.Top, ks.Right - ks.Left, ks.Bottom - ks.Top);
             int frame = 0;
             string filename;
 
-            Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
-            var pt = new Point(bounds.Left, bounds.Top);
-            Graphics g = Graphics.FromImage(bitmap);
+            //Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height);
+            //var pt = new Point(bounds.Left, bounds.Top);
+            //Graphics g = Graphics.FromImage(bitmap);
             string[][] stringframe = new string[6572][];
-            int bufsize = 10000 - 1;
-            Console.SetBufferSize(bufsize, bufsize);
-
-
+            long curprg = 0;
+            var locker = new object();
             Parallel.For(1, 6572, (i) =>
             {
                 filename = $"badapplefr\\{i:D10}.jpg";
@@ -87,22 +170,23 @@ namespace BMPneed2beASCENDED
                         }
                     }
                 }
+                Interlocked.Increment(ref curprg);
+                Console.Title = $"touhou realtime preloading.... {Interlocked.Read(ref curprg)}/6572";                
             });
 
             int garo = bufsize / width /2;
-
-
-            Console.Title = "touhou realtime readying frames : " + frame.ToString();
+            Console.Title = "touhou realtime preloading....";
             for (int i=0;i< stringframe.Length/garo; i++)
             {
                 for(int j = 0; j < height; j++)
                 {
-                    for(int k = 0; k < garo; k++)
+                    int k;
+                    for (k = 0; k < garo; k++)
                     {
                         Console.Write(stringframe[garo * i + k][j]);
                     }
+                    Console.Title = $"touhou realtime predrawing.... {garo * i}-{garo * (i+1)}/6572";
                     Console.WriteLine();
-                    //Thread.Sleep(1000);
                 }
             }
             
@@ -115,18 +199,25 @@ namespace BMPneed2beASCENDED
             */
             //Console.Title = "touhou realtime playing: " + frame.ToString();
             int curframe = 0;
-            Console.ReplaceAllColorsWithDefaults();
-            Console.BackgroundColor = Color.White;
+            var sp = new System.Media.SoundPlayer("badsong.wav");
+            //sp.PlaySync();
+            sp.Load();
+            sp.Play();
+            Thread.Sleep(1600);
             Stopwatch sk = Stopwatch.StartNew();
-            while (curframe<stringframe.Length)
+            while (curframe < stringframe.Length)
             {
-                Console.CursorTop = height * (curframe / garo);
-                Console.CursorLeft = width * (curframe % garo);
+                if (height * (curframe / garo) + 23 > 9990) break;
+                Console.WindowTop = height * (curframe / garo) + 23;
+                Console.WindowLeft = (width * (curframe % garo)) * 2;
+                //Thread.Sleep(1000);
                 //Console.Clear();
                 //Console.Write(stringframe[curframe]);
                 Console.Title = "touhou realtime playing : " + curframe.ToString();
                 curframe = (int)(sk.ElapsedMilliseconds * 30.0 / 1000.0);
             }
+            
+            
             /*
             psi = new ProcessStartInfo("G:\\ffmpeg\\bin\\ffmpeg",
                     $"-i {filename} tmpsound.aac");
@@ -164,7 +255,7 @@ namespace BMPneed2beASCENDED
         public static void consoleimageoutput(Bitmap dat)
         {
             //Console.ResetColor();
-            Console.ReplaceAllColorsWithDefaults();
+            //Console.ReplaceAllColorsWithDefaults();
             const char thatchar = '■';
             string buf = "";
             Color lstclr = dat.GetPixel(0, 0);
